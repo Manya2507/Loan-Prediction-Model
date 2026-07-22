def544871f1d855da3947ba93d101100ac27d96a
Manya2507 <manyasingla2507@gmail.com>
2026-07-22 10:27:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ demo = gr.Interface(
 # Launch
 # ==========================================================
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     demo.launch(
         server_name="0.0.0.0",
         server_port=int(os.environ.get("PORT", 7860))
